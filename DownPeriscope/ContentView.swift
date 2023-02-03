a123db30e70d2f6cf5c82c3157d27433f0f53bf3
Mike Gray <mgray88@gmail.com>
2023-02-03 17:28:29 -0500

    Initial Commit

---
+++ DownPeriscope/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DownPeriscope
+//
+//  Created by Mike Gray on 2/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DownPeriscope/DownPeriscopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownPeriscopeApp.swift
+//  DownPeriscope
+//
+//  Created by Mike Gray on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DownPeriscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
